2aeb105a5ece5b869dae6ea663bcf969c5870422
Larik7 <Larik777taganka@gmail.com>
2023-05-24 08:21:20 +0300

    second commit, router with contacts

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,17 +1,25 @@
 const express = require('express')
 
+const Joi = require("joi");
+
 const contactsService = require("../../models/contacts/contacts");
 
+const { HttpError } = require("../../helpers");
+
 const router = express.Router()
+
+const contactAddSchema = Joi.object({
+    "name": Joi.string().required(),
+    "email": Joi.string().required(),
+    "phone": Joi.string().required()
+})
 
 router.get('/', async (req, res, next) => {
   try {
     const result = await contactsService.listContacts();
     res.json(result)
   } catch (error) {
-    res.status(500).json({
-      message: "Server error"
-    })
+      next(error);
   }
 })
 
@@ -19,21 +27,32 @@ router.get('/:id', async (req, res, next) => {
   try {
     const {id} = req.params;
     const result = await contactsService.getContactById(id);
-    if(!result) {
-      res.status(404).json({
-      message: `Contact with ${id} not found`
-      })
+    if (!result) {
+      throw HttpError(404, `Contact with ${id} not found`);
+      // const error = new Error(`Contact with ${id} not found`);
+      // error.status = 404;
+      // throw error;
+      // res.status(404).json({
+      // message: `Contact with ${id} not found`
+      // })
     }
   res.json(result);
   } catch (error) {
-    res.status(500).json({
-      message: "Server error"
-    })
+      next(error);
   }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { error } = contactAddSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message)
+    }
+    const result = await contactsService.addContact(req.body);
+    res.status(201).json(result)
+  } catch (error) {
+      next(error)
+  }
 })
 
 router.delete('/:id', async (req, res, next) => {
