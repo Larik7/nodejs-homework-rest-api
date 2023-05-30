d013336d03d40ac507a8aade63a6590f96883f7b
Larik7 <Larik777taganka@gmail.com>
2023-05-31 00:57:17 +0300

    fix server

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-const { DB_HOST } = ("./config");
-
-mongoose
-  .connect(DB_HOST)
+const { DB_HOST } = require("./config");
+mongoose.connect(DB_HOST)
   .then(() => {
     app.listen(3000, () => {
       console.log("Database connection successful");
