fa8e41153e856cabcca5af67cc52bf45ee62c262
Larik7 <Larik777taganka@gmail.com>
2023-05-31 02:21:37 +0300

    try to deploy

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-const DB_HOST =
-  "mongodb+srv://Larik:UdPnuu4BET3Fff1t@cluster0.dosr8jl.mongodb.net/my_contacts?retryWrites=true&w=majority";
+// const DB_HOST =
+//   "mongodb+srv://Larik:UdPnuu4BET3Fff1t@cluster0.dosr8jl.mongodb.net/my_contacts?retryWrites=true&w=majority";
 
-module.exports = {
-  DB_HOST,
-}
+// module.exports = {
+//   DB_HOST,
+// }
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-const { DB_HOST } = require("./config");
+// const { DB_HOST } = require("./config");
+const DB_HOST =
+  "mongodb+srv://Larik:UdPnuu4BET3Fff1t@cluster0.dosr8jl.mongodb.net/my_contacts?retryWrites=true&w=majority";
 mongoose.connect(DB_HOST)
   .then(() => {
-    app.listen(3000, () => {
-      console.log("Database connection successful");
-    });
+    app.listen(3000);
   })
-  .catch((error) => {
+  .catch(error => {
     console.log(error.message);
     process.exit(1);
   });
