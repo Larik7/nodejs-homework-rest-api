88d1ec23dd18eee6e005458be54de2b449d4dfb5
Larik7 <larik777taganka@gmail.com>
2023-05-30 16:38:35 +0300

    connect to mongodb

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+const mongoose = require("mongoose");
 const app = require('./app')
 
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
 })
+
+// UdPnuu4BET3Fff1t
+// mongodb+srv://Larik:<password>@cluster0.dosr8jl.mongodb.net/
