79edfb6bc17372defef3a9a34c08df01c46040ea
Larik7 <Larik777taganka@gmail.com>
2023-06-02 01:18:44 +0300

    add config.js and test

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-// const { DB_HOST } = require("./config");
-const DB_HOST =
-  "mongodb+srv://Larik:UdPnuu4BET3Fff1t@cluster0.dosr8jl.mongodb.net/my_contacts?retryWrites=true&w=majority";
+const { DB_HOST } = require("./config");
+
 mongoose
   .connect(DB_HOST)
   .then(() => {
