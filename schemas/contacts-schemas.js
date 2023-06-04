df0adf3db7e7e33cdbe82212c78227e5fff3455f
Larik7 <Larik777taganka@gmail.com>
2023-06-05 02:21:59 +0300

    add schema mongoose

--- schemas/contacts-schemas.js
+++ schemas/contacts-schemas.js
@@ -1,18 +1,51 @@
 const Joi = require("joi");
+const { Schema, model } = require("mongoose");
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
 
 const contactAddSchema = Joi.object({
-  name: Joi.string().required().messages({
-    'any.required': "missing required name field"
-  }),
-  email: Joi.string().email().required().messages({
-    'any.required': "missing required name field",
-    'any.invalid': 'invalid value'
-  }),
-  phone: Joi.string().required().messages({
-    'any.required': "missing required name field"
-  })
+  name: Joi.string()
+    .regex(/^[\p{L}\s]+$/u)
+    .min(3)
+    .max(30)
+    .trim()
+    .required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .required(),
+  phone: Joi.string()
+    .regex(/^[\d\-+\s()]+$/)
+    .min(10)
+    .max(15)
+    .trim()
+    .required(),
+  favorite: Joi.boolean().required(),
 });
 
+const Contact = model("contact", contactSchema);
+
 module.exports = {
-    contactAddSchema,
-}
+  Contact,
+  contactAddSchema,
+};
